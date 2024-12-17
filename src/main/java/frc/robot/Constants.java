21290bf32afdac4bbcb489734c73f126a79a03ad
Jesse Kane <jessekane66@gmail.com>
2024-12-17 17:30:17 -0500

    Added auton test path

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -40,6 +40,11 @@ public final class Constants {
                     public static final double kP = 0.1;
                     public static final double kI = 0.0;
                     public static final double kD = 0.0;
+
+                    //Feedforward constants
+                    public static final double kV = 0.0;
+                    public static final double kS = 0.0;
+                    public static final double kA = 0.0;
                 }
     
                 public static class Turn {
--- src/main/java/frc/robot/subsystems/drive/Module.java
+++ src/main/java/frc/robot/subsystems/drive/Module.java
@@ -1,5 +1,50 @@
 package frc.robot.subsystems.drive;
 
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.robot.subsystems.Drive.ModuleIOInputsAutoLogged;
+import frc.robot.Constants.DriveConstants.ModuleConstants.Common.Drive;
+
 public class Module {
-    
+    private final ModuleIO io;
+    private final String ID;
+    private final ModuleIOInputsAutoLogged inputs = new ModuleIOInputsAutoLogged();
+    private final SimpleMotorFeedforward driveFFController;
+
+    public Module(ModuleIO io, String ID){
+        this.io = io;
+        this.ID = ID;
+        driveFFController = new SimpleMotorFeedforward(
+            Drive.kS,
+            Drive.kV,
+            Drive.kA,
+            0.2
+        );
+    }
+
+    /*Sets desired state in closed-loop mode */
+    public void setDesiredState(SwerveModuleState state){
+        io.updateInputs(inputs); //Fetches latest data from IO layer
+        state.optimize(inputs.turnPosition);
+        io.setDriveVelocity(
+            state.speedMetersPerSecond, 
+            driveFFController.calculate(MetersPerSecond.of(state.speedMetersPerSecond)).in(Volts)
+        );
+        io.setTurnPosition(
+            state.angle.getRotations(), 
+            0
+        );
+    }
+
+    /*Must be manually called by DriveSubsystem */
+    public void periodic(){
+        io.updateInputs(inputs);
+        Logger.processInputs("Drive/" + ID, inputs);
+    }
 }
