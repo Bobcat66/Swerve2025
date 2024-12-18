bebfeae8e931e9c65bb2056a25c052043150bed6
Jesse Kane <jessekane66@gmail.com>
2024-12-17 19:49:58 -0500

    Implemented Pathplanner

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -25,9 +27,34 @@ public final class Constants {
 
     public static class DriveConstants {
         public static final int odometryFrequencyHz = 250;
+        public static final double wheelBase = 0.5; //Meters
+        public static final double trackWidth = 0.5; //Meters
+        public static final Translation2d[] moduleTranslations = new Translation2d[] {
+            new Translation2d(trackWidth / 2.0, wheelBase / 2.0),
+            new Translation2d(trackWidth / 2.0, -wheelBase / 2.0),
+            new Translation2d(-trackWidth / 2.0, wheelBase / 2.0),
+            new Translation2d(-trackWidth / 2.0, -wheelBase / 2.0)
+        };
+
         public static class GyroConstants {
             public static final int kGyroPort = -1;
         }
+
+        public static class AutoConstants {
+            public static class PIDControl {
+                public static class Trans {
+                    public static final double kP = 5.0;
+                    public static final double kI = 0.0;
+                    public static final double kD = 0.0;
+                }
+                public static class Rot {
+                    public static final double kP = 5.0;
+                    public static final double kI = 0.0;
+                    public static final double kD = 0.0;
+                }
+            }
+        }
+
         public static class ModuleConstants {
             public static class Common {
                 public static class Drive {
@@ -35,6 +62,7 @@ public final class Constants {
                     public static final double VoltageCompensation = 12;
                     public static final double PositionConversionFactor = 1.0;
                     public static final double VelocityConversionFactor = 1.0;
+                    public static final double MaxModuleSpeed = 14.0; //Maximum attainable module speed
 
                     //PID constants
                     public static final double kP = 0.1;
@@ -61,23 +89,21 @@ public final class Constants {
             }
             public static enum ModuleConfig {
     
-                FrontLeft(1,11,21,0.0,"FrontLeftModule"),
-                FrontRight(2,12,22,0.0,"FrontRightModule"),
-                RearLeft(3,13,23,0.0,"RearLeftModule"),
-                RearRight(4,14,24,0.0,"RearRightModule");
+                FrontLeft(1,11,21,0.0),
+                FrontRight(2,12,22,0.0),
+                RearLeft(3,13,23,0.0),
+                RearRight(4,14,24,0.0);
     
                 public final int DrivePort;
                 public final int TurnPort;
                 public final int EncoderPort;
                 public final double EncoderOffsetRots;
-                public final String ID;
     
-                private ModuleConfig(int DrivePort, int TurnPort,int EncoderPort,double EncoderOffsetRots,String ID) {
+                private ModuleConfig(int DrivePort, int TurnPort,int EncoderPort,double EncoderOffsetRots) {
                     this.DrivePort = DrivePort;
                     this.TurnPort = TurnPort;
                     this.EncoderPort = EncoderPort;
                     this.EncoderOffsetRots = EncoderOffsetRots;
-                    this.ID = ID;
                 }
             }
         }
--- src/main/java/frc/robot/subsystems/drive/Module.java
+++ src/main/java/frc/robot/subsystems/drive/Module.java
@@ -7,15 +7,17 @@ import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import frc.robot.subsystems.Drive.ModuleIOInputsAutoLogged;
 import frc.robot.Constants.DriveConstants.ModuleConstants.Common.Drive;
+import frc.robot.subsystems.Drive.ModuleIOInputsAutoLogged;
 
 public class Module {
     private final ModuleIO io;
     private final String ID;
     private final ModuleIOInputsAutoLogged inputs = new ModuleIOInputsAutoLogged();
     private final SimpleMotorFeedforward driveFFController;
+    private SwerveModulePosition[] odometryModulePositions = new SwerveModulePosition[] {};
 
     public Module(ModuleIO io, String ID){
         this.io = io;
@@ -42,9 +44,49 @@ public class Module {
         );
     }
 
+    /*Sets desired state with overridden Feedforward */
+    public void setDesiredState(SwerveModuleState state, double driveFFVolts, double turnFFVolts){
+        io.updateInputs(inputs); //Fetches latest data from IO layer
+        state.optimize(inputs.turnPosition);
+        io.setDriveVelocity(
+            state.speedMetersPerSecond, 
+            driveFFVolts
+        );
+        io.setTurnPosition(
+            state.angle.getRotations(), 
+            turnFFVolts
+        );
+    }
+
+    public void stop(){
+        io.setDriveVolts(0.0);
+        io.setTurnVolts(0.0);
+    }
+
+    public double[] getOdometryTimestamps(){
+        return inputs.odometryTimestamps;
+    }
+
+    public SwerveModulePosition getPosition(){
+        return new SwerveModulePosition(inputs.drivePositionMeters,inputs.turnPosition);
+    }
+
+    public SwerveModuleState getState(){
+        return new SwerveModuleState(inputs.driveVelocityMetersPerSec,inputs.turnPosition);
+    }
+
+    public SwerveModulePosition[] getOdometryModulePositions(){
+        return odometryModulePositions;
+    }
+
     /*Must be manually called by DriveSubsystem */
     public void periodic(){
         io.updateInputs(inputs);
         Logger.processInputs("Drive/" + ID, inputs);
+        int sampleCount = inputs.odometryDrivePositionsMeters.length;
+        odometryModulePositions = new SwerveModulePosition[sampleCount];
+        for (int i = 0; i < sampleCount; i++){
+            odometryModulePositions[i] = new SwerveModulePosition(inputs.odometryDrivePositionsMeters[i],inputs.odometryTurnPositions[i]);
+        }
     }
 }
