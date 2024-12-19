c7528f114a6df18a4405ecf2999ef72a24ec1969
Jesse Kane <jessekane66@gmail.com>
2024-12-19 11:12:21 -0500

    firstcommit

---
+++ src/main/java/frc/robot/subsystems/drive/DriveIO.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.drive;
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+
+public interface DriveIO {
+
+    @AutoLog
+    public static class DriveIOInputs {
+        
+    }
+    
+}
---
+++ src/main/java/frc/robot/subsystems/drive/DriveIOCTRE.java
@@ -0,0 +1,4 @@
+package frc.robot.subsystems.drive;
+import com.ctre.phoenix6.swerve.SwerveDrivetrain;
+
+public class DriveIOCTRE extends SwerveDrivetrain
